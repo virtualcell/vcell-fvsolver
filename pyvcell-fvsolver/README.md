5d191f623064ccf8fd2cca66bc50df633a77c6f6
jcschaff <schaff.jim@gmail.com>
2024-06-02 10:38:32 -0400

    separate scikit-build-core build using 'pip install ./pyvcell_fvsolver'

---
+++ pyvcell-fvsolver/README.md
@@ -0,0 +1,1 @@
+## pyvcell_fvsolver
---
+++ pyvcell-fvsolver/src/main.cpp
@@ -0,0 +1,42 @@
+#include <../../extern/pybind11/include/pybind11/pybind11.h>
+
+#include "SolverMain.h"
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_core, m) {
+    m.doc() = R"pbdoc(
+        VCell FiniteVolume solver
+        -------------------------
+
+        .. currentmodule:: pyvcell_fvsolver
+
+        .. autosummary::
+           :toctree: _generate
+
+           version
+           solve
+    )pbdoc";
+
+    m.def("version", &version, R"pbdoc(
+        version of build
+
+        version string of build using git hash
+    )pbdoc");
+
+    m.def("solve", &solve, R"pbdoc(
+        solve the PDE
+
+        The inputFilename expects a .fvinput file, the outputDir will be created as needed.
+    )pbdoc",
+        py::arg("inputFilename"), py::arg("outputDir"));
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
