57a78ea35acc3db3ea8cfdbd8d8f165031395bca
Logan Drescher <drescher@uchc.edu>
2025-03-28 13:16:42 -0400

    Added Smoldyn Smoke test, and corresponding discovered NPE Fault
    Fault was caused by trying to access a null-mols reference. The reference was actually populated, but the VCellSmoldynOutput was constructed with an old simptr, and the proper pointer was being provided through function calls, but ignored.

--- VCell/tests/TestVCell.cpp
+++ VCell/tests/TestVCell.cpp
@@ -1,8 +1,12 @@
 #include <gtest/gtest.h>
 #include <filesystem>
 #include <fstream>
+#include <cstring>  // Note the c++ variant, not the .h original
+#include <cstdlib>  // Note the c++ variant, not the .h original
 
 #include <VCELL/SolverMain.h>
+
+#include "smoldynfuncs.h"
 #include "../build/VCell/tests/testFiles/input/testResourceLocations.h" // provides testResourceLocations
 
 
@@ -15,5 +19,26 @@ TEST(VCellTest, ExcerciseFV) {
 }
 
 TEST(VCellTest, ExerciseSmoldyn) {
-    int retcode_1;
+    std::string first_arg{"ignored"};
+    std::string second_arg{testResourceLocations::EXAMPLE_SMOLDYN_INPUT};
+    // Allocate zero-initialized memory
+    char* first_arg_ptr = static_cast<char*>(std::calloc(first_arg.size() + 1, sizeof(char)));
+    char* second_arg_ptr = static_cast<char*>(std::calloc(second_arg.size() + 1, sizeof(char)));
+    ASSERT_TRUE(first_arg_ptr && second_arg_ptr) << "Memory allocation failed";
+
+    std::strcpy(first_arg_ptr, first_arg.c_str());
+    std::strcpy(second_arg_ptr, second_arg.c_str());
+    char* arg_array[2];
+    arg_array[0] = first_arg_ptr;
+    arg_array[1] = second_arg_ptr;
+
+    int retcode_1 = runSmoldyn(2, arg_array);
+    EXPECT_EQ(retcode_1, 0);
+    int retcode_2 = runSmoldyn(2, arg_array);
+    EXPECT_EQ(retcode_2, 0);
+
+    std::free(first_arg_ptr);
+    std::free(second_arg_ptr);
 }
+
+
